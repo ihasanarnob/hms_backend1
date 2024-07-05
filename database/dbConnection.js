c6459df2dd576bce50f9f5b37f490b48cc7245bc
ihasanarnob <hasanarnob3@gmail.com>
2024-07-05 23:14:35 +0600

    first commit

---
+++ database/dbConnection.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export const dbConnection = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "MERN_HOSPITAL_SYSTSEM_DEPLOYED",
+    })
+    .then(() => {
+      console.log("Connected to database!");
+    })
+    .catch((err) => {
+      console.log("Some error arrived while connecting to database:", err);
+    });
+};
