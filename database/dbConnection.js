033b1dc68fb599650a7a5ede2e4526c90880a1e1
ihasanarnob <hasanarnob3@gmail.com>
2024-07-11 19:54:07 +0600

    port-change

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const dbConnection = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
-      dbName: "MERN_HOSPITAL_SYSTSEM_DEPLOYED",
+      dbName: "MERN_HOSPITAL_SYSTSEM",
     })
     .then(() => {
       console.log("Connected to database!");
